1b340408e67d9a43bd0189acbe278d47fdc73e87
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-09-23 04:01:13 +0530

    auth.controller.js setup

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,6 +3,13 @@ import express from "express";
 
 const authRoutes = express.Router();
 
+authRoutes.post("/register")
+
+authRoute.post("/login")
+
+authRoutes.post("/logout")
+
+authRoutes.get("/check")
 
 
 export default authRoutes;
