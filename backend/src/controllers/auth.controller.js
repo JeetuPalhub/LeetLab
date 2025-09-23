d0828d1fc54ffe4745789f0f49a8509e70cb603f
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-09-23 19:00:46 +0530

    authentication done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -146,9 +146,13 @@ export const check = async (req, res) => {
         res.status(200).json({
             success:true,
             message:"User authenticated successfully",
-        })
+            user:req.user
+        });
     } catch (error) {
-        
+        console.error("Error checking user:", error);
+        res.status(500).json({
+            error:"Error checking user"
+        })
     }
 }
 
