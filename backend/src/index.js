1a253580ff0f15e9be873f2d3572c9cfe9d454b9
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-09-23 16:24:28 +0530

    register, login , logout controller created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
@@ -7,6 +11,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+
 
 app.get("/", (req,res) => {
     res.send("Hello Guys Welcome to leetlab🔥");
